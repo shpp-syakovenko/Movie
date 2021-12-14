f6c5368b801b826c1baac4e4bd3ef1419ee605eb
Sergey <serglife777@gmail.com>
2021-12-14 21:37:38 +0200

    Get favorites

--- app/src/main/java/com/serglife/movie/data/database/DataBaseMovie.kt
+++ app/src/main/java/com/serglife/movie/data/database/DataBaseMovie.kt
@@ -3,7 +3,10 @@ package com.serglife.movie.data.database
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.serglife.movie.domain.entity.Movie
@@ -12,28 +15,27 @@ class DataBaseMovie {
     private lateinit var database: DatabaseReference
     private lateinit var auth: FirebaseAuth
 
-    suspend fun getFavorites(): List<Movie> {
-        database =
-            Firebase.database("https://movie-c8b47-default-rtdb.europe-west1.firebasedatabase.app").reference
+    fun getFavorites(): List<Movie> {
+        database = Firebase.database("https://movie-c8b47-default-rtdb.europe-west1.firebasedatabase.app").reference
         auth = Firebase.auth
 
         val list = mutableListOf<Movie>()
 
-        database.child(auth.uid.toString()).child("movies").get()
+        database.child(auth.uid.toString()).child("movies")
+            .addListenerForSingleValueEvent(object : ValueEventListener{
 
-            .addOnSuccessListener { dataSnapshot ->
-
-                dataSnapshot.children.forEach {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.children.forEach {
                     val movie = it.getValue(Movie::class.java)
                         ?: throw RuntimeException("Not to convert!!!!!!!!!")
-                    Log.i("FAVORITES", "${movie.title}")
                     list.add(movie)
                 }
-
             }
-            .addOnFailureListener {
 
-            }
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+
 
         return list
 
