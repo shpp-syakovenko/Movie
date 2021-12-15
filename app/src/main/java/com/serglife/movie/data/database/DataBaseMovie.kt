410d204620a63ab61e159cd8a2013794209bb155
Sergey <serglife777@gmail.com>
2021-12-15 13:20:53 +0200

    getFavorites(): refactoring

--- app/src/main/java/com/serglife/movie/data/database/DataBaseMovie.kt
+++ app/src/main/java/com/serglife/movie/data/database/DataBaseMovie.kt
@@ -1,6 +1,5 @@
 package com.serglife.movie.data.database
 
-import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
@@ -10,36 +9,35 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.serglife.movie.domain.entity.Movie
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class DataBaseMovie {
     private lateinit var database: DatabaseReference
     private lateinit var auth: FirebaseAuth
 
-    fun getFavorites(): List<Movie> {
-        database = Firebase.database("https://movie-c8b47-default-rtdb.europe-west1.firebasedatabase.app").reference
-        auth = Firebase.auth
+    suspend fun getFavorites(): List<Movie> {
+        return suspendCoroutine { continuation ->
 
-        val list = mutableListOf<Movie>()
+            database = Firebase.database.reference
+            auth = Firebase.auth
 
-        database.child(auth.uid.toString()).child("movies")
-            .addListenerForSingleValueEvent(object : ValueEventListener{
+            database.child(auth.uid.toString()).child("movies")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
 
-            override fun onDataChange(snapshot: DataSnapshot) {
-                snapshot.children.forEach {
-                    val movie = it.getValue(Movie::class.java)
-                        ?: throw RuntimeException("Not to convert!!!!!!!!!")
-                    list.add(movie)
-                }
-            }
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val list = snapshot.children.map {
+                            it.getValue(Movie::class.java)
+                                ?: throw RuntimeException("Not to convert!!!!!!!!!")
+                        }
+                        continuation.resume(list)
+                    }
 
-            override fun onCancelled(error: DatabaseError) {}
+                    override fun onCancelled(error: DatabaseError) {
+                        continuation.resume(listOf())
+                    }
 
-        })
-
-
-        return list
-
+                })
+        }
     }
 }
-
-
--- app/src/main/java/com/serglife/movie/presentation/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/serglife/movie/presentation/ui/favorites/FavoritesFragment.kt
@@ -6,6 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.serglife.movie.R
 import com.serglife.movie.databinding.FragmentFavoritesBinding
 import com.serglife.movie.domain.entity.Movie
@@ -21,6 +29,9 @@ class FavoritesFragment : Fragment(), OnClickFavoritesListener {
     private val vm by viewModel<FavoritesViewModel>()
     private lateinit var adapter: FavoritesAdapter
 
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,10 +42,11 @@ class FavoritesFragment : Fragment(), OnClickFavoritesListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initField()
+        getFavorites()
 
-        vm.movies.observe(viewLifecycleOwner,{listMovie ->
+/*        vm.movies.observe(viewLifecycleOwner,{listMovie ->
             adapter.submitList(listMovie)
-        })
+        })*/
     }
 
     private fun initField() {
@@ -45,6 +57,30 @@ class FavoritesFragment : Fragment(), OnClickFavoritesListener {
 
     override fun onClick(movie: Movie) {
         findNavController().navigate(FavoritesFragmentDirections.actionFavoritesFragmentToDetailFragment(movie))
+    }
+
+    private fun getFavorites() {
+        database = Firebase.database("https://movie-c8b47-default-rtdb.europe-west1.firebasedatabase.app").reference
+        auth = Firebase.auth
+
+        val list = mutableListOf<Movie>()
+
+        database.child(auth.uid.toString()).child("movies")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    snapshot.children.forEach {
+                        val movie = it.getValue(Movie::class.java)
+                            ?: throw RuntimeException("Not to convert!!!!!!!!!")
+
+                        list.add(movie)
+                    }
+                    adapter.submitList(list)
+                }
+                override fun onCancelled(error: DatabaseError) {}
+
+            })
+
     }
 
 
